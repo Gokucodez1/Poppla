0932df713863534007e38d5bbeb6a1b220afd7f6
Gokucodez1 <133681723+Gokucodez1@users.noreply.github.com>
2025-07-04 10:10:46 +0600

    Create sochain.py

---
+++ sochain.py
@@ -0,0 +1,16 @@
+import requests
+
+def check_ltc_transaction(address):
+    url = f"https://sochain.com/api/v2/get_address_balance/LTC/mainnet/ {address}"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        
+        if float(data["data"]["received_value"]) > 0:
+            txid = data["data"]["transactions"][0]
+            return {"success": True, "txid": txid}
+            
+        return {"success": False}
+    
+    except Exception as e:
+        return {"success": False, "error": str(e)}
